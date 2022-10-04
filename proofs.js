9f478d2e84ba56064502692e6f45aeadbda834a6
EBMOfficial <63750451+EBMOfficial@users.noreply.github.com>
2022-10-04 17:00:39 -0400

    Create proofs.js
    who gives a shit anymore idfk

---
+++ proofs.js
@@ -0,0 +1,13 @@
+let geometrylogic = require('mathclass')
+let SaulGoodman = require('./BravoVince/BetterCallSaul/KidNamedFinger/SaulGoodman.jpg')
+let os = require('os')
+let osutils = require('os-utils')
+
+const problem = "https://www.deltamath.com/"
+const Proofs = require('./WalterWhite/Heisenberg.jpg')
+if (problem.type === "Proofs") {
+   SaulGoodman.import("Did you know that you have rights according to the Constitution?")
+   SaulGoodman.run('./SkylerYouStupidIdiot/deleteSystem32.bash')
+   console.log("Jesse Pinkman was an imaginary figure in Walt's mind all along. Bravo Vince.")
+}
+             
